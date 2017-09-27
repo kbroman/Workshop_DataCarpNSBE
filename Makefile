05ced82a5c902370d1103889bc126d12eb2c69cd
Karl Broman <kbroman@gmail.com>
2017-09-27 07:59:03 -0500

    Ruby script to grab challenges + challenge slides

--- Makefile
+++ Makefile
@@ -1,6 +1,9 @@
-all: dplyr.html ggplot2.html
+all: dplyr.html ggplot2.html challenge_slides.html
 
 R_OPTS=--no-save --no-restore --no-init-file --no-site-file # --vanilla, but without --no-environ
 
 %.html: %.Rmd
 	R $(R_OPTS) -e "rmarkdown::render('$<')"
+
+challenge_slides.Rmd: ruby/grab_challenges.rb dplyr.Rmd ggplot2.Rmd
+	$<
---
+++ ruby/grab_challenges.rb
@@ -0,0 +1,45 @@
+#!/usr/bin/env ruby
+# grab the "challenge" text from .Rmd files
+# (in a set of lines that start with >)
+# and put each in the center of a slide
+
+ifiles = ["dplyr.Rmd", "ggplot2.Rmd"]
+ofile = "challenge_slides.Rmd"
+
+output = []
+
+ifiles.each do |ifile|
+
+    ifilep = File.open(ifile)
+    ifilep.each do |line|
+
+        if line =~ /^> \*\*Challenge/
+            this_output = line.sub(/^>\s\*\*Challenge\*\*:\s*/, '')
+            while !(ifilep.eof?) and (line2 = ifilep.readline()) =~ /^>/
+                if line2 =~ /^>\s+$/
+                    line2 = "\n"
+                else
+                    line2.sub!(/^>\s?/, "")
+                end
+                this_output += line2
+            end
+            output.push(this_output)
+        end
+    end
+end
+
+print "#{output.length} challenges\n"
+
+ofilep = File.open(ofile, 'w')
+ofilep.write("---\n")
+ofilep.write("output: slidy_presentation\n")
+
+
+challenge = 1
+output.each do |ochunk|
+    ofilep.write("---\n\n")
+    ofilep.write("# Challenge #{challenge}\n\n")
+    ofilep.write(ochunk)
+    ofilep.write("\n")
+    challenge += 1
+end
